d55708e1ca109202c959d5d3beb21b13046fb13d
“verbol” <verbol96@mail.ru>
2023-05-24 17:47:51 +0300

    7

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -2,8 +2,8 @@ import axios from "axios"
 
 const $host = axios.create({
     withCredentials: true,
-    baseURL: 'http://localhost:8001/' //для local
-    //baseURL: 'http://94.228.126.26:8001/' // для server
+    //baseURL: 'http://localhost:8001/' //для local
+    baseURL: 'http://85.193.91.221:8001/' // для server
 })
 
 $host.interceptors.request.use((config) => {
@@ -19,7 +19,7 @@ $host.interceptors.response.use(config =>
         if (error.response.status === 401 && error.config && !error.config._isRetry) {
             originalRequest._isRetry = true;
             try {
-                const response = await axios.get('http://localhost:8000/api/auth/refresh', {withCredentials: true})
+                const response = await axios.get('http://85.193.91.221:8001/api/auth/refresh', {withCredentials: true})
                 localStorage.setItem('token', response.data.accessToken);
                 return $host.request(originalRequest);
             } catch (e) {
