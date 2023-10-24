76daa6b90dbfbf0cad21611a3e61b92695dc6d8c
“verbol” <verbol96@mail.ru>
2023-10-24 18:40:35 +0300

    16

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -48,7 +48,7 @@ class authController {
             
 
             const accessToken = jwt.sign({ "id": user.id, "phone": user.phone}, process.env.ACCESS_KEY, {expiresIn: '10s'})
-            const refreshToken = jwt.sign({ "id": user.id, "phone": user.phone}, process.env.REFRESH_KEY, {expiresIn: '72h'})
+            const refreshToken = jwt.sign({ "id": user.id, "phone": user.phone}, process.env.REFRESH_KEY, {expiresIn: '20s'})
             
 
             await Token.destroy({where: {userId: user.id}})
